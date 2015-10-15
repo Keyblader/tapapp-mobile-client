67c121252383eda20146c4c084167b6f93f0d931
Alberto <p02diada@gmail.com>
2015-10-15 10:42:24 +0200

    Estructura básica de la aplicacion con algunos controladores y URLs

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,8 +1,35 @@
-var app = angular.module('TapApp', []);
+angular.module('app.controllers', [])
+  
 
-app.controller('Hello', function($scope, $http) {
+
+  
+.controller('inicioCtrl', function($scope, $http) {
 	$http.get('http://kaerzas.pythonanywhere.com/tapas/listaTapas/').
     success(function(data) {
         $scope.greeting = data;
-    });
-});
+    })
+})
+ 
+.controller('anyadirBarCtrl', function($scope) {
+
+})  
+
+.controller('seleccionarBarCtrl', function($scope) {
+
+}) 
+   
+.controller('anyadirBarPorTapaCtrl', function($scope) {
+
+})
+
+//.controller('detalleTapa', function($scope) {
+
+//})
+
+.controller('detalleTapa', function($scope, $http) {
+	$http.get('http://kaerzas.pythonanywhere.com/tapas/detalleTapa/1/').
+    success(function(data) {
+        $scope.greeting = data;
+    })
+}) 
+
