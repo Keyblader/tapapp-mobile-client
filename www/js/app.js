cef0db38f752b9d55b23ef72ed0c0d7e1ed86472
Pablo Castro <pcastrolora@gmail.com>
2015-10-18 10:59:26 +0200

    app.js y controller.js reestructurados para tolerar un menú. Menú añadido Parche de estilos CSS para ionic añadido.

--- www/js/app.js
+++ www/js/app.js
@@ -3,74 +3,133 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-// 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('app', ['ionic', 'app.controllers', 'app.services', 'app.directives'])
+angular.module('starter', ['ionic', 'starter.controllers'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
-    if(window.cordova && window.cordova.plugins.Keyboard) {
+    if (window.cordova && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.disableScroll(true);
+
     }
-    if(window.StatusBar) {
+    if (window.StatusBar) {
       // org.apache.cordova.statusbar required
       StatusBar.styleDefault();
     }
   });
 })
 
-
 .config(function($stateProvider, $urlRouterProvider) {
-
-  // Ionic uses AngularUI Router which uses the concept of states
-  // Learn more here: https://github.com/angular-ui/ui-router
-  // Set up the various states which the app can be in.
-  // Each state's controller can be found in controllers.js
   $stateProvider
+
+    .state('app', {
+    url: '/app',
+    abstract: true,
+    templateUrl: 'templates/menu.html',
+    controller: 'AppCtrl'
+  })
+
+  /*.state('app.search', {
+    url: '/search',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/search.html'
+      }
+    }
+  })
+
+  .state('app.browse', {
+      url: '/browse',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/browse.html'
+        }
+      }
+    })
+    .state('app.playlists', {
+      url: '/playlists',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/playlists.html',
+          controller: 'PlaylistsCtrl'
+        }
+      }
+    })
+
+  .state('app.single', {
+    url: '/playlists/:playlistId',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/playlist.html',
+        controller: 'PlaylistCtrl'
+      }
+    }
+  })*/
+  
+  // PROPIOS //
     
-      
-        
-    .state('inicio', {
+      .state('app.inicio', {
       url: '/inicio',
-      templateUrl: 'templates/inicio.html',
-      controller: 'inicioCtrl'
+      views: {
+          'menuContent': {
+		      templateUrl: 'templates/inicio.html',
+		      controller: 'inicioCtrl'
+          }
+      }
     })
         
-    .state('anyadirBar', {
+    .state('app.anyadirBar', {
       url: '/anyadirBar',
-      templateUrl: 'templates/anyadirBar.html',
-      controller: 'anyadirBarCtrl'
+      views: {
+          'menuContent': {
+		      templateUrl: 'templates/anyadirBar.html',
+		      controller: 'anyadirBarCtrl'
+          }
+      }
     })  
-    .state('anyadirTapa', {
+    .state('app.anyadirTapa', {
       url: '/anyadirTapa',
-      templateUrl: 'templates/anyadirTapa.html',
-      controller: 'anyadirTapaCtrl'
+      views: {
+          'menuContent': {
+		      templateUrl: 'templates/anyadirTapa.html',
+		      controller: 'anyadirTapaCtrl'
+          }
+      }
     })  
-      .state('seleccionarBar', {
+      .state('app.seleccionarBar', {
       url: '/seleccionarBar',
-      templateUrl: 'templates/seleccionarBar.html',
-      controller: 'seleccionarBarCtrl'
+      views: {
+          'menuContent': {
+		      templateUrl: 'templates/seleccionarBar.html',
+		      controller: 'seleccionarBarCtrl'
+          }
+      }
     })  
 
-    .state('anyadirBarPorTapa', {
+    .state('app.anyadirBarPorTapa', {
       url: '/anyadirBarPorTapa',
-      templateUrl: 'templates/anyadirBarPorTapa.html',
-      controller: 'anyadirBarPorTapaCtrl'
+      views: {
+          'menuContent': {
+		      templateUrl: 'templates/anyadirBarPorTapa.html',
+		      controller: 'anyadirBarPorTapaCtrl'
+          }
+      }
     })      
         
-    .state('detalleTapa', {
+    .state('app.detalleTapa', {
       url: '/detalleTapa/:id',
-      templateUrl: 'templates/detalleTapa.html',
-      controller: 'detalleTapaCtrl'
-    })      
-
-        
-      
-    ;
-
+      views: {
+          'menuContent': {
+		      templateUrl: 'templates/detalleTapa.html',
+		      controller: 'detalleTapaCtrl'
+          }
+      }
+    })  
+  
+  ;
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/inicio');
-
+  $urlRouterProvider.otherwise('/app/inicio');
 });
