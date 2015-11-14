421e877e08b49b44a1d0540e7e6a58e23cd353cf
i22balur <i22balur@i22balur-K53SD>
2015-11-15 00:07:50 +0100

    Unificacion de plugins

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -16,21 +16,6 @@ angular.module('starter.controllers', ['starter.services'])
 			"longitud":$scope.longitude,
 		}
 
-
-
-
-		
-
-		//$http.post('http://kaerzas.pythonanywhere.com/api-token-auth/', 'username=' + user.username + '&password=' + user.password, {
-		//	  headers: {
-		//	    'Content-Type': 'application/x-www-form-urlencoded'
-		//	  }
-		//})
-	    //.success(function(data) {
-	    //	sharedToken.setProperty(data.token);
-	        //console.log(sharedToken.getProperty())
-	    //	window.location = "#/app/inicio";
-	    //})
 		console.log(sharedToken.getProperty());
 
 		$http({method: 'GET', url: 'http://kaerzas.pythonanywhere.com/tapas/listaTapas/', params: {latitud:$scope.latitude, longitud:$scope.longitude, rango:$scope.user.value},headers: {
@@ -280,82 +265,7 @@ angular.module('starter.controllers', ['starter.services'])
 		$scope.showAlert();
 	}
 	
-/*
-	$controller('PhotoController', {$scope: $scope});//Usar un controlador desde otro controlador
-	$scope.bar = {}
 
-	$http.get('http://kaerzas.pythonanywhere.com/usuarios/dameUsuario/', {
-		  headers: {
-			  'Authorization': 'Token ' + sharedToken.getProperty()
-		  }
-	})
-	.success(function(data) {
-		$scope.usuario = data.user;
-	})
-	
-	//función que se ejecuta al obtener la posición con getCurrentPosition si no hay error
-	var onSuccess = function(position) {
-		$scope.latitude=position.coords.latitude;
-		$scope.longitude=position.coords.longitude;
-
-		//para cargar los parametros del mapa
-		var latlng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-		var mapSettings = {
-				center: latlng,
-				zoom: 15,
-				mapTypeId: google.maps.MapTypeId.ROADMAP
-		}
-		//para crear el mapa y dibujarlo en el div
-		var map = new google.maps.Map(document.getElementById('mapa'), mapSettings);
-
-		var marker = new google.maps.Marker({
-			position: latlng,
-			map: map,
-			draggable: false,
-			title: 'Arrastrame'
-		});
-	};
-
-//	onError Callback receives a PositionError object
-
-	function onError(error) {
-		alert('code: '    + error.code    + '\n' +
-				'message: ' + error.message + '\n');
-	};
-
-	navigator.geolocation.getCurrentPosition(onSuccess, onError);		
-
-	$scope.update = function() {
-		console.log($scope.bar);
-	};
-
-	$scope.reset = function(barForm) {
-		$scope.bar = {};
-	};
-
-	$scope.guardar= function (bar) {
-
-
-		var b = {
-				"nombre": bar.nombre,
-				"descripcion": bar.descripcion,
-				"longitud": $scope.longitude,
-				"latitud": $scope.latitude,
-				"imagen" : $scope.imgURI,
-				"fechaSubida": new Date(),//Fecha actual
-				"usuarioRegistro": $scope.usuario
-		}
-
-		$http.post('http://kaerzas.pythonanywhere.com/tapas/anyadirBar/', b, {
-			headers: {
-				'Authorization': 'Token ' + sharedToken.getProperty()
-			}
-		})
-		.success(function(data) {
-			window.location = "#/app/inicio";
-		})
-	};
-	*/
 })
 
 
